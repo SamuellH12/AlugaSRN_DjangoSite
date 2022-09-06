4364d9744238615dfd38d9439ae08cf49df21fec
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2022-09-06 08:20:37 -0300

    os.path.join removed from settings (possible error)

--- oxenteNet/settings.py
+++ oxenteNet/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 
